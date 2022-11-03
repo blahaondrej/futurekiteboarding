5c15f2cedec232c9560992af64e4f60c32007214
oblaha <blaha.ondra@gmail.com>
2022-11-03 14:52:27 +0100

    contact styled email

--- contact.php
+++ contact.php
@@ -44,10 +44,217 @@ require 'phpmailer/src/SMTP.php';
 
 	$subject = "FUTURE kiteboarding - contact form";
 
-		$body = "Your message from the FUTUREkiteboarding.com website: <br><br><br>";
-		$body .= "Your name: " . $name . "<br>";
-    	$body .= "Your e-mail: " . $email . "<br>";
-    	$body .= "Message: " . $comment . "<br><br>";
+		$body = '
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <style>
+        body, p, td, tr, span {
+            padding: 0;
+            margin: 0;
+            font-family: \'Arial\', sans-serif;
+            font-style: italic;
+        }
+    </style>
+</head>
+<body>
+<table align="center" cellpadding="0" cellspacing="0" border="0" bgcolor="#fff" width="750">
+	<tr>
+		<td>
+			<table align="center" width="750" cellpadding="0" cellspacing="0" border="0" bgcolor="#fff">
+				<tr>
+					<td>
+						<a href="https://futurekiteboarding.com/">
+							<img src="https://www.kitelementshop.com/assets/app/images/emails/intro.jpg"
+								 alt="FUTURE Kiteboarding" style="width: 100%; height: auto;">
+						</a>
+					</td>
+				</tr>
+			</table>
+		</td>
+	</tr>
+</table>
+<table align="center" cellpadding="0" cellspacing="0" border="0" bgcolor="#fff" width="750">
+		<tr>
+			<td>
+				<table align="center" width="650" cellpadding="0" cellspacing="0" border="0" bgcolor="#fff">
+					<tr>
+						<td height="30">
+							&nbsp;
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<span style="font-size: 72px; color: #F71106; font-style: italic; font-weight: 600; letter-spacing: 0; line-height: 86px;">THE BALL IS IN OUR COURT…</span>
+						</td>
+					</tr>
+					<tr>
+						<td height="40">
+							&nbsp;
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<span style="color: #030303; font-size: 36px; font-style: italic; font-weight: 600; letter-spacing: 0; line-height: 40px;">Unless we are at the sea or working on a new collection, we are probably about to answer your questions.</span>
+						</td>
+					</tr>
+					<tr>
+						<td height="30">
+							&nbsp;
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<span style="color: #030303; font-size: 21px; font-style: italic; font-weight: 300; letter-spacing: 0.56px; line-height: 24px;">Thanks for your message, we will respond as soon as possible. If you cannot or don\'t want to wait, you can contact us via whatsapp or look up the FAQs.</span>
+						</td>
+					</tr>
+					<tr>
+						<td height="60">
+							&nbsp;
+						</td>
+					</tr>
+				</table>
+			</td>
+		</tr>
+	</table>
+	<table align="center" cellpadding="0" cellspacing="0" border="0" bgcolor="#fff" width="650">
+	<tr>
+		<td height="50" style="border-top: 1px solid #1A1A1A;">
+			&nbsp;
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<span style="color: #030303; font-size: 36px; font-style: italic; font-weight: 600; letter-spacing: 0.96px; line-height: 40px;">GOT ANY MORE QUESTIONS?</span>
+		</td>
+	</tr>
+	<tr>
+		<td height="30">
+			&nbsp;
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<table>
+				<tr>
+					<td width="135">
+						<img src="https://www.kitelementshop.com/assets/app/images/emails/email.png"
+							 alt="FUTURE Kiteboarding" style="width: 100%; height: auto;">
+					</td>
+					<td width="50">
+						&nbsp;
+					</td>
+					<td>
+						<a href="mailto:info@futurekiteboarding.com"
+						   style="color: #F71106; font-size: 21px; font-style: italic; font-weight: bold; letter-spacing: 0.56px; line-height: 66px; text-transform: uppercase; text-decoration: none;">INFO@FUTUREKITEBOARDING.COM</a>
+					</td>
+				</tr>
+			</table>
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<table>
+				<tr>
+					<td width="135">
+						<img src="https://www.kitelementshop.com/assets/app/images/emails/whatsapp.png"
+							 alt="FUTURE Kiteboarding" style="width: 100%; height: auto;">
+					</td>
+					<td width="50">
+						&nbsp;
+					</td>
+					<td>
+						<a href="https://wa.me/420775526626"
+						   style="color: #F71106; font-size: 21px; font-style: italic; font-weight: bold; letter-spacing: 0.56px; line-height: 66px; text-transform: uppercase; text-decoration: none;">WHATSAPP</a>
+					</td>
+				</tr>
+			</table>
+		</td>
+	</tr>
+	<tr>
+		<td height="40">
+			&nbsp;
+		</td>
+	</tr>
+</table>
+<table align="center" cellpadding="0" cellspacing="0" border="0" bgcolor="#fff" width="750">
+	<tr>
+		<td>
+			<table align="center" width="750" cellpadding="0" cellspacing="0" border="0" bgcolor="#1A1A1A">
+				<tr>
+					<td>
+						<table align="center" width="600" cellpadding="0" cellspacing="0" border="0" bgcolor="#1A1A1A">
+							<tr>
+								<td height="30">
+									&nbsp;
+								</td>
+							</tr>
+							<tr>
+								<td align="center">
+									<span style="color: #FFFFFF; font-size: 21px; font-style: italic; font-weight: 300; letter-spacing: 0.56px; line-height: 40px; text-align: center;">Follow us and stay updated on new products and other exciting stuff.</span>
+								</td>
+							</tr>
+							<tr>
+								<td height="30">
+									&nbsp;
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<table align="center">
+										<tr>
+											<td width="84">
+												<a href="https://www.instagram.com/futurekiting/">
+													<img src="https://www.kitelementshop.com/assets/app/images/emails/instagram.png"
+														 alt="FUTURE Kiteboarding" style="width: 100%; height: auto;">
+												</a>
+											</td>
+											<td width="30">
+												&nbsp;
+											</td>
+											<td width="84">
+												<a href="https://www.facebook.com/futurekiteboarding">
+													<img src="https://www.kitelementshop.com/assets/app/images/emails/facebook.png"
+														 alt="FUTURE Kiteboarding" style="width: 100%; height: auto;">
+												</a>
+											</td>
+											<td width="30">
+												&nbsp;
+											</td>
+											<td width="84">
+												<a href="https://www.youtube.com/channel/UCoBM9F04NllSO8Hk4KDZUeA">
+													<img src="https://www.kitelementshop.com/assets/app/images/emails/youtube.png"
+														 alt="FUTURE Kiteboarding" style="width: 100%; height: auto;">
+												</a>
+											</td>
+										</tr>
+									</table>
+								</td>
+							</tr>
+							<tr>
+								<td height="30">
+									&nbsp;
+								</td>
+							</tr>
+							<tr>
+								<td align="center">
+									<span style="color: #FFFFFF; font-size: 12px; font-style: italic; font-weight: 300; letter-spacing: 0.32px; line-height: 20px;">ALL RIGHTS RESERVED BY  FUTURE kiteboarding 2022 ©</span>
+								</td>
+							</tr>
+						</table>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<img src="https://www.kitelementshop.com/assets/app/images/emails/footer.jpg"
+							 alt="FUTURE Kiteboarding" style="width: 100%; height: auto;">
+					</td>
+				</tr>
+			</table>
+		</td>
+	</tr>
+</table>';
 
 $mail = new PHPMailer(true);
 
